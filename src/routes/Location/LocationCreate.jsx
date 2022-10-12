288be06ce2afcb1f1ad88a743b08f174d95ba6d7
marga <margaritazerpa18@gmail.com>
2022-10-11 23:46:50 -0300

    actualización de estilos form

--- src/routes/Location/LocationCreate.jsx
+++ src/routes/Location/LocationCreate.jsx
@@ -1,8 +1,11 @@
 import React from "react";
-import './LocationCreate.css'
+import "./LocationCreate.css";
+import { LocationContext } from "../../contexts/LocationContext";
+
 const LocationCreate = () => {
+  const { locations } = useContext(LocationContext);
   return (
-    <div class="d-flex justify-content-center">
+    <div id="container" class="d-flex justify-content-center">
       <form class="card col-sm-4 col-md-3 p-5">
         <h3 class="display-10 text-dark text-center">CREAR TARJETA</h3>
         <div class="mb-3">
@@ -14,28 +17,19 @@ const LocationCreate = () => {
             class="form-control"
             id="InputNombre"
             aria-describedby="nombreHelp"
-            placeholder=""
           />
         </div>
         <div class="mb-3">
           <label for="InputLongitud" class="form-label">
             Longitud
           </label>
-          <input
-            type="longitud"
-            class="form-control"
-            id="InputLongitud"
-          />
+          <input type="longitud" class="form-control" id="InputLongitud" />
         </div>
         <div class="mb-3">
           <label for="InputLatitud" class="form-label">
             Latitud
           </label>
-          <input
-            type="latitud"
-            class="form-control"
-            id="InputLatitud"
-          />
+          <input type="latitud" class="form-control" id="InputLatitud" />
         </div>
         <button type="submit" class="btn btn-dark">
           Crear
