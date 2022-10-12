53562356b73f66295befe22d6d1da1f0c2d40b4d
marga <margaritazerpa18@gmail.com>
2022-10-12 09:13:24 -0300

    se capturan datos del form

--- src/routes/Location/LocationCreate.jsx
+++ src/routes/Location/LocationCreate.jsx
@@ -1,39 +1,83 @@
-import React from "react";
+import { useContext, useState, useEffect } from "react";
 import "./LocationCreate.css";
 import { LocationContext } from "../../contexts/LocationContext";
 
+const initForm = {
+  id: null,
+  name: "",
+  latitude: "",
+  longitude: "",
+  temperature: "",
+  windspeed: "",
+  img: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/PlazaBelgrano1.jpg/280px-PlazaBelgrano1.jpg",
+};
+
 const LocationCreate = () => {
-  const { locations } = useContext(LocationContext);
+  const [form, setForm] = useState(initForm);
+
+  const createData = (data) => {
+    console.log(data);
+  };
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!form.name || !form.latitude || !form.longitude) {
+      alert("Datos incompletos");
+      return;
+    }
+    createData(form);
+  };
+
+  const handleReset = (e) => {
+    setForm(initForm);
+  };
+
   return (
-    <div id="container" class="d-flex justify-content-center">
-      <form class="card col-sm-4 col-md-3 p-5">
-        <h3 class="display-10 text-dark text-center">CREAR TARJETA</h3>
-        <div class="mb-3">
-          <label for="InputNombre" class="form-label">
-            Nombre de Ciudad
-          </label>
+    <div id="container" className="d-flex justify-content-center">
+      <form className="card col-sm-4 col-md-3 p-5" onSubmit={handleSubmit}>
+        <h3 className="display-10 text-dark text-center">CREAR TARJETA</h3>
+        <div className="mb-3">
           <input
-            type="nombre"
-            class="form-control"
-            id="InputNombre"
-            aria-describedby="nombreHelp"
+            size="40"
+            type="text"
+            name="name"
+            placeholder="Nombre de Ciudad"
+            onChange={handleChange}
+            value={form.name}
+            className="form-control"
           />
         </div>
-        <div class="mb-3">
-          <label for="InputLongitud" class="form-label">
-            Longitud
-          </label>
-          <input type="longitud" class="form-control" id="InputLongitud" />
+        <div className="mb-3">
+          <input
+            size="40"
+            type="text"
+            name="longitude"
+            placeholder="Longitud"
+            onChange={handleChange}
+            value={form.longitude}
+            className="form-control"
+          />
         </div>
-        <div class="mb-3">
-          <label for="InputLatitud" class="form-label">
-            Latitud
-          </label>
-          <input type="latitud" class="form-control" id="InputLatitud" />
+        <div className="mb-3">
+          <input
+            size="40"
+            type="text"
+            name="latitude"
+            placeholder="Latitud"
+            onChange={handleChange}
+            value={form.latitude}
+            className="form-control"
+          />
         </div>
-        <button type="submit" class="btn btn-dark">
-          Crear
-        </button>
+        <input type="submit" value="Crear" />
+        <input type="reset" value="Limpiar" onClick={handleReset} />
       </form>
     </div>
   );
